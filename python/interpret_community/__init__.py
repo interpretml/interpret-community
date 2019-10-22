a1946ca1455479642b379edf50815f63d32d3960
Ilya Matiach <ilmat@microsoft.com>
2019-10-22 13:53:42 -0400

    add infrastructure to create a logging file on import if an env variable is specified

--- python/interpret_community/__init__.py
+++ python/interpret_community/__init__.py
@@ -11,3 +11,12 @@ confidence in the model.
 from .tabular_explainer import TabularExplainer
 
 __all__ = ["TabularExplainer"]
+
+# Setup logging infrustructure
+import logging
+import os
+# Only log to disk if environment variable specified
+interpretc_logs = os.environ.get('INTERPRETC_LOGS')
+if interpretc_logs is not None:
+    logging.basicConfig(filename=interpretc_logs, level=logging.INFO)
+    logging.info('Initializing logging file for interpret-community')
