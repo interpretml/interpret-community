6de7b19ecea3c8ec7c2252bf75938ebd3f180fb3
Ilya Matiach <ilmat@microsoft.com>
2019-11-19 16:10:35 -0500

    fix logging logic if directory for file is missing

---
+++ test/test_logging.py
@@ -0,0 +1,26 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Tests the logging mechanism in the library"""
+
+import os
+
+
+def test_import_tabular():
+    not_existing_dir = 'not_existing_dir'
+    not_existing_path = not_existing_dir + '/interpret_community_log.txt'
+    if os.path.exists(not_existing_path):
+        os.remove(not_existing_path)
+        os.rmdir(not_existing_dir)
+    os.environ['INTERPRETC_LOGS'] = not_existing_path
+    import interpret_community
+    import importlib
+    importlib.reload(interpret_community)
+    assert(os.path.exists(not_existing_path))
+
+    try:
+        os.remove(not_existing_path)
+        os.rmdir(not_existing_dir)
+    except PermissionError:
+        pass
