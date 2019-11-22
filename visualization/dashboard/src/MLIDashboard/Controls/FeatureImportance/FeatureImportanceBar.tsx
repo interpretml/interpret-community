aa5634f097379e966db0495db23191167fa466a7
Brandon Horn <rihorn@microsoft.com>
2019-11-22 11:22:37 -0500

    Theme charts (#113)
    Pass theme arg to chart components, accepting 'light' | 'dark' | 'white' | 'black' now

--- visualization/dashboard/src/MLIDashboard/Controls/FeatureImportance/FeatureImportanceBar.tsx
+++ visualization/dashboard/src/MLIDashboard/Controls/FeatureImportance/FeatureImportanceBar.tsx
@@ -113,6 +113,7 @@ export class FeatureImportanceBar extends React.PureComponent<IGlobalFeatureImpo
                         </Callout>
                         )}
                     <BarChart 
+                        theme={this.props.theme}
                         intercept={this.props.dashboardContext.explanationContext.globalExplanation.intercepts}
                         featureByClassMatrix={featuresByClassMatrix}
                         sortedIndexVector={sortVector}
