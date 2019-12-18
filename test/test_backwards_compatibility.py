25fd851762c49d061dafe257fc2935db4e294ee3
Ilya Matiach <ilmat@microsoft.com>
2019-12-18 15:33:52 -0500

    fix backcompat for explain local and global with data

--- test/test_backwards_compatibility.py
+++ test/test_backwards_compatibility.py
@@ -6,9 +6,12 @@
 
 import json
 from os import path
+import pandas as pd
+import numpy as np
+from datasets import retrieve_dataset
 
 
-def test_model_backcompat(mimic_explainer):
+def test_model_backcompat_global(mimic_explainer):
     class DummyModel:
         def predict(self):
             return
@@ -27,4 +30,27 @@ def test_model_backcompat(mimic_explainer):
     # Use the surrogate model as the original model
     explainer.model = explainer.surrogate_model
     global_explanation = explainer.explain_global()
+    assert len(global_explanation.global_importance_values) == explainer.surrogate_model.model._n_features
+
+
+def test_model_backcompat_local(mimic_explainer):
+    class DummyModel:
+        def predict(self, X):
+            return X['TotalBalance']
+
+    dummy_model = DummyModel()
+    model_file = 'old_mimic_model2.json'
+    if not path.exists(model_file):
+        model_file = path.join('test', model_file)
+    with open(model_file, 'r') as file:
+        data = file.read()
+    properties = json.loads(data)
+    explainer = mimic_explainer._load(dummy_model, properties)
+    eval_data = retrieve_dataset('backcompat_data.csv')
+    df = pd.DataFrame(np.random.randint(0, eval_data.shape[0], size=(eval_data.shape[0], 674 - 5)))
+    eval_data = eval_data[eval_data.columns[-5:]]
+    eval_data = pd.concat([df, eval_data], axis=1)
+    local_explanation = explainer.explain_local(eval_data)
+    assert local_explanation._local_importance_values.shape[1] == explainer.surrogate_model.model._n_features
+    global_explanation = explainer.explain_global(eval_data)
     assert len(global_explanation.global_importance_values) == explainer.surrogate_model.model._n_features
