c5fb8aff51b1b8b875be3f660feed718df3cf6a5
bethz <6098674+bethz@users.noreply.github.com>
2019-12-20 13:29:22 -0500

    add notebook tests

---
+++ test/test_notebooks.py
@@ -0,0 +1,105 @@
+import papermill as pm
+import scrapbook as sb
+import pytest
+
+
+@pytest.mark.notebooks
+def test_explain_binary_classification_local():
+
+    notebookname = "explain-binary-classification-local"
+    input_notebook = "notebooks/" + notebookname + ".ipynb"
+    output_notebook = "./test/" + notebookname + ".output.ipynb"
+
+    pm.execute_notebook(input_notebook, output_notebook)
+
+    nb = sb.read_notebook(input_notebook)
+    nb.scraps  # print a dict of all scraps by name
+
+    return
+
+
+@pytest.mark.notebooks
+def test_explain_regression_local():
+
+    notebookname = "explain-regression-local"
+    input_notebook = "notebooks/" + notebookname + ".ipynb"
+    output_notebook = "./test/" + notebookname + ".output.ipynb"
+
+    pm.execute_notebook(input_notebook, output_notebook)
+
+    nb = sb.read_notebook(input_notebook)
+    bn = sb.read_notebook(output_notebook)
+    print(nb.scraps)  # print a dict of all scraps by name
+    print(nb.scrap_dataframe)
+    print(bn.scraps)
+    print(bn.scrap_dataframe)
+    jj=bn.scrap_dataframe
+    print('scrap', bn.scraps[1])
+    print("local imp is ", jj[[1]])
+    
+    assert False
+
+    return
+
+@pytest.mark.notebooks
+def test_read_sb():
+    notebookname = "explain-regression-local"
+    input_notebook = "notebooks/" + notebookname + ".ipynb"
+    output_notebook = "./test/" + notebookname + ".output.ipynb"
+    bn = sb.read_notebook(output_notebook)
+    print('hey')
+    print(bn.scraps)
+    print(bn.scrap_dataframe)
+    print("local imp is ", bn.scraps.data_dict['local_imp'])
+    if 'AGE' in bn.scraps.data_dict['local_imp']:
+        print('found age')
+        assert True
+    else:
+        assert False
+
+    return
+
+
+@pytest.mark.notebooks
+def test_advanced_feature_transformations_explain_local():
+
+    notebookname = "advanced-feature-transformations-explain-local"
+    input_notebook = "notebooks/" + notebookname + ".ipynb"
+    output_notebook = "./test/" + notebookname + ".output.ipynb"
+
+    pm.execute_notebook(input_notebook, output_notebook)
+
+    nb = sb.read_notebook(input_notebook)
+    nb.scraps  # print a dict of all scraps by name
+
+    return
+
+
+@pytest.mark.notebooks
+def test_explain_multiclass_classification_local():
+
+    notebookname = "explain-multiclass-classification-local"
+    input_notebook = "notebooks/" + notebookname + ".ipynb"
+    output_notebook = "./test/" + notebookname + ".output.ipynb"
+
+    pm.execute_notebook(input_notebook, output_notebook)
+
+    nb = sb.read_notebook(input_notebook)
+    nb.scraps  # print a dict of all scraps by name
+
+    return
+
+
+@pytest.mark.notebooks
+def test_simple_feature_transformations_explain_local():
+
+    notebookname = "simple-feature-transformations-explain-local"
+    input_notebook = "notebooks/" + notebookname + ".ipynb"
+    output_notebook = "./test/" + notebookname + ".output.ipynb"
+
+    pm.execute_notebook(input_notebook, output_notebook)
+
+    nb = sb.read_notebook(input_notebook)
+    nb.scraps  # print a dict of all scraps by name
+
+    return
