ceca5e8fb8e9ba0c55b2e45f5733636a10d4c42f
bethz <6098674+bethz@users.noreply.github.com>
2020-01-03 19:41:40 -0500

    NB Test, common env template and other test updates (#143)
    * add common template to create env
    
    * update PR-Gate with template to create env
    
    * add notebook markers, template, use python -m pytest
    
    * add notebook tests
    - add sample test to test output of notebook

---
+++ test/test_notebooks.py
@@ -0,0 +1,92 @@
+import papermill as pm
+import scrapbook as sb
+import pytest
+
+
+@pytest.mark.notebooks
+def test_explain_binary_classification_local():
+
+    notebookname = "explain-binary-classification-local"
+    input_notebook = "notebooks/" + notebookname + ".ipynb"
+    output_notebook = "./test/" + notebookname + ".output.ipynb"
+
+    pm.execute_notebook(input_notebook, output_notebook)
+
+    nb = sb.read_notebook(output_notebook)
+    nb.scraps  # print a dict of all scraps by name
+
+    return
+
+
+@pytest.mark.notebooks
+def test_explain_regression_local():
+
+    notebookname = "explain-regression-local"
+    input_notebook = "notebooks/" + notebookname + ".ipynb"
+    output_notebook = "./test/" + notebookname + ".output.ipynb"
+
+    pm.execute_notebook(input_notebook, output_notebook)
+    nb = sb.read_notebook(output_notebook)
+    print(nb.scraps)  # print a dict of all scraps by name
+    print(nb.scrap_dataframe)
+    assert True
+
+    return
+
+
+@pytest.mark.notebooks
+def test_read_sb():
+    notebookname = "explain-regression-local"
+    input_notebook = "notebooks/" + notebookname + ".ipynb"
+    output_notebook = "./test/" + notebookname + ".output.ipynb"
+
+    pm.execute_notebook(input_notebook, output_notebook)
+    bn = sb.read_notebook(output_notebook)
+
+    assert "AGE" in bn.scraps.data_dict["local_imp"]
+    return
+
+
+@pytest.mark.notebooks
+def test_advanced_feature_transformations_explain_local():
+
+    notebookname = "advanced-feature-transformations-explain-local"
+    input_notebook = "notebooks/" + notebookname + ".ipynb"
+    output_notebook = "./test/" + notebookname + ".output.ipynb"
+
+    pm.execute_notebook(input_notebook, output_notebook)
+
+    nb = sb.read_notebook(output_notebook)
+    nb.scraps  # print a dict of all scraps by name
+
+    return
+
+
+@pytest.mark.notebooks
+def test_explain_multiclass_classification_local():
+
+    notebookname = "explain-multiclass-classification-local"
+    input_notebook = "notebooks/" + notebookname + ".ipynb"
+    output_notebook = "./test/" + notebookname + ".output.ipynb"
+
+    pm.execute_notebook(input_notebook, output_notebook)
+
+    nb = sb.read_notebook(output_notebook)
+    nb.scraps  # print a dict of all scraps by name
+
+    return
+
+
+@pytest.mark.notebooks
+def test_simple_feature_transformations_explain_local():
+
+    notebookname = "simple-feature-transformations-explain-local"
+    input_notebook = "notebooks/" + notebookname + ".ipynb"
+    output_notebook = "./test/" + notebookname + ".output.ipynb"
+
+    pm.execute_notebook(input_notebook, output_notebook)
+
+    nb = sb.read_notebook(output_notebook)
+    nb.scraps  # print a dict of all scraps by name
+
+    return
