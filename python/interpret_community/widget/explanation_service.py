3effe3cb9cbd7e914801c4dc5d796b7e67b7e1b9
Brandon Horn <rihorn@microsoft.com>
2020-01-06 16:34:07 -0500

    configuarable port, use cdn

--- python/interpret_community/widget/explanation_service.py
+++ python/interpret_community/widget/explanation_service.py
@@ -1,6 +1,7 @@
 from gevent.pywsgi import WSGIServer
 from flask import Flask, render_template, url_for, request
-from IPython.display import display, HTML
+from IPython.display import display, HTML, IFrame
+from gevent.pywsgi import WSGIServer
 import threading
 import jinja2
 import os
@@ -14,9 +15,15 @@ class ExplanationDashboard:
     
     class DashboardService:
         app = Flask(__name__)
-        
-        app.config['TESTING'] = True
-        app.config["EXPLAIN_TEMPLATE_LOADING"] = True
+
+        def __init__(self, port):
+            self.port = port
+            self.ip = '127.0.0.1'
+
+        def run(self):
+            server =  WSGIServer((self.ip, self.port), self.app)
+            self.app.config["server"] = server
+            server.serve_forever()
 
         @app.route('/')
         def hello():
@@ -24,31 +31,44 @@ class ExplanationDashboard:
 
         @app.route('/<id>')
         def explanation_visual(id):
-            if id in LocalExplanationDash.explanations:
-                return render_template( 'dashboard.html', explanation=json.dumps(LocalExplanationDash.explanations[id].dashboard_input), main_js='http://127.0.0.1:5000/static/index.js', app_id='app_123')
+            # if there is no internet or the static file exists, use that
+            # main_js='http://127.0.0.1:5000/static/index.js'
+            if id in ExplanationDashboard.explanations:
+                return render_template( 'dashboard.html', explanation=json.dumps(ExplanationDashboard.explanations[id].dashboard_input), main_js='https://responsible-ai.azureedge.net/index.js', app_id='app_123')
             else:
                 return "unknown..."
         
         @app.route('/<id>/predict', methods=['POST'])
         def predict(id):
             data = request.get_json(force=True)
-            if id in LocalExplanationDash.explanations:
-                return LocalExplanationDash.explanations[id].on_predict(data)
+            if id in ExplanationDashboard.explanations:
+                return ExplanationDashboard.explanations[id].on_predict(data)
 
-    def __init__(self, explanationObject, model=None, *, datasetX=None, trueY=None, classes=None, features=None):
-        if not LocalExplanationDash.__service:
-            LocalExplanationDash.__service = LocalExplanationDash.DashboardService()
-            self._thread = threading.Thread(target=LocalExplanationDash.__service.app.run, daemon=True)
+    def __init__(self, explanationObject, model=None, *, datasetX=None, trueY=None, classes=None, features=None, port=5000):
+        if not ExplanationDashboard.__service:
+            ExplanationDashboard.__service = ExplanationDashboard.DashboardService(port)
+
+            self._thread = threading.Thread(target=ExplanationDashboard.__service.run, daemon=True)
             self._thread.start()
-        LocalExplanationDash.model_count += 1
-        predict_url = "http://127.0.0.1:5000/{0}/predict".format(str(LocalExplanationDash.model_count))
-        LocalExplanationDash.explanations[str(LocalExplanationDash.model_count)] = ExplanationDashboardInput(explanationObject, model, datasetX, trueY, classes, features, predict_url)
+        ExplanationDashboard.model_count += 1
+        predict_url = "http://{0}:{1}/{2}/predict".format(
+            ExplanationDashboard.__service.ip,
+            ExplanationDashboard.__service.port,
+            str(ExplanationDashboard.model_count))
+        ExplanationDashboard.explanations[str(ExplanationDashboard.model_count)] = ExplanationDashboardInput(explanationObject, model, datasetX, trueY, classes, features, predict_url)
 
-        html = "<iframe src='http://127.0.0.1:5000/{0}' width='100%' height='800px' frameBorder='0'></iframe>".format(LocalExplanationDash.model_count)
+        html = "<iframe src='http://{0}:{1}/{2}' width='100%' height='800px' frameBorder='0'></iframe>".format(
+            ExplanationDashboard.__service.ip,
+            ExplanationDashboard.__service.port,
+            ExplanationDashboard.model_count)
         if "DATABRICKS_RUNTIME_VERSION" in os.environ:
             _render_databricks(html)
         else: 
-            display(HTML(html))
+            url = 'http://{0}:{1}/{0}'.format(
+                ExplanationDashboard.__service.ip,
+                ExplanationDashboard.__service.port,
+                ExplanationDashboard.model_count)
+            display(IFrame(url, "100%", 1200))
     
 # NOTE: Code mostly derived from Plotly's databricks render as linked below:
 # https://github.com/plotly/plotly.py/blob/01a78d3fdac14848affcd33ddc4f9ec72d475232/packages/python/plotly/plotly/io/_base_renderers.py
