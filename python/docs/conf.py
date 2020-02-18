f739aa926070bef94317aebf8d296b7431a5fba4
Walter Martin <42586848+wamartin-aml@users.noreply.github.com>
2020-02-18 14:37:40 -0500

    update sphinx conf file (#165)

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -36,8 +36,17 @@ extensions = [
     'sphinx.ext.extlinks'
 ]
 
+# enable links to objects in the other standard libraries, e.g., list and str in the Python standard library
+intersphinx_mapping = {
+    'Python': ('https://docs.python.org/3', None),
+    'NumPy': ('http://docs.scipy.org/doc/numpy/', None),
+    'pandas': ('http://pandas.pydata.org/pandas-docs/stable/', None),
+    'SciPy': ('http://docs.scipy.org/doc/scipy/reference', None),
+    'sklearn': ('http://scikit-learn.org/stable', None)
+}
+
 # eventually we may be able to use intersphinx for these! TODO
-autodoc_mock_imports = ['shap', 'interpret']
+autodoc_mock_imports = ['shap', 'shap.common', 'interpret', 'interpret.utils']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,6 +55,10 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# http://www.sphinx-doc.org/en/master/usage/configuration.html
+# master doctree document
+master_doc = 'index'
 
 
 # -- Options for HTML output -------------------------------------------------
