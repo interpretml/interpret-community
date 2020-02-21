c56148ab89e3f08085b55661f9da906a8da8b37a
Marc Gelormino <v-magelo@microsoft.com>
2020-02-21 19:35:27 +0100

    Fix import as suggested by review.
    Signed-off-by: Marc Gelormino <v-magelo@microsoft.com>

--- python/interpret_community/community/__init__.py
+++ python/interpret_community/community/__init__.py
@@ -4,6 +4,6 @@
 
 """Module for model interpretability, including feature and class importance for blackbox and greybox models."""
 
-from interpret_community import TabularExplainer
+from interpret_community.tabular_explainer import TabularExplainer
 
 __all__ = ["TabularExplainer"]
