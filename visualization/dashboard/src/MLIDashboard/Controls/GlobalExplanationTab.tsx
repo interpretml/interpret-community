9d0a72650c08819206d0e3e0e76681595cd8fed1
Brandon Horn <rihorn@microsoft.com>
2020-02-24 15:49:18 -0500

    wip

--- visualization/dashboard/src/MLIDashboard/Controls/GlobalExplanationTab.tsx
+++ visualization/dashboard/src/MLIDashboard/Controls/GlobalExplanationTab.tsx
@@ -8,9 +8,14 @@ import { localization } from "../../Localization/localization";
 import _ from "lodash";
 import { DependencePlot } from "./DependencePlot";
 import { IGenericChartProps } from "../NewExplanationDashboard";
+import { mergeStyleSets } from "@uifabric/styling";
+import { SpinButton } from "office-ui-fabric-react/lib/SpinButton";
+import { Slider } from "office-ui-fabric-react/lib/Slider";
+import { ModelExplanationUtils } from "../ModelExplanationUtils";
 
 export interface IGlobalBarSettings {
     topK: number;
+    startingK: number;
     sortOption: string;
     includeOverallGlobal: boolean;
     sortIndexVector: number[];
@@ -33,22 +38,81 @@ export interface IGlobalExplanationTabProps {
 }
 
 export class GlobalExplanationTab extends React.PureComponent<IGlobalExplanationTabProps> {
+    private static readonly classNames = mergeStyleSets({
+        page: {
+            display: "contents"
+        },
+        globalChartControls: {
+            display: "flex",
+            flexDirection: "row"
+        },
+        topK: {
+            maxWidth: "200px"
+        },
+        startingK: {
+            flex: 1
+        }
+    });
+    
     constructor(props: IGlobalExplanationTabProps) {
         super(props);
         if (this.props.globalBarSettings === undefined) {
             this.setDefaultSettings(props);
         }
+        this.setStartingK = this.setStartingK.bind(this);
     }
     public render(): React.ReactNode {
         if (this.props.globalBarSettings === undefined) {
             return (<div/>);
         }
+        const minK = Math.min(4, this.props.jointDataset.localExplanationFeatureCount);
+        const maxK = Math.min(30, this.props.jointDataset.localExplanationFeatureCount);
+        const maxStartingK = Math.max(0, this.props.jointDataset.localExplanationFeatureCount - this.props.globalBarSettings.topK);
+        const relayoutArg = {'xaxis.range': [this.props.globalBarSettings.startingK - 0.5, this.props.globalBarSettings.startingK + this.props.globalBarSettings.topK - 0.5]};
         const plotlyProps = this.buildBarPlotlyProps();
-        return(<>
+        _.set(plotlyProps, 'layout.xaxis.range', [this.props.globalBarSettings.startingK - 0.5, this.props.globalBarSettings.startingK + this.props.globalBarSettings.topK - 0.5]);
+        return (
+        <div className={GlobalExplanationTab.classNames.page}>
+            <div className={GlobalExplanationTab.classNames.globalChartControls}>
+                <SpinButton
+                    className={GlobalExplanationTab.classNames.topK}
+                    styles={{
+                        spinButtonWrapper: {maxWidth: "150px"},
+                        labelWrapper: { alignSelf: "center"},
+                        root: {
+                            display: "inline-flex",
+                            float: "right",
+                            selectors: {
+                                "> div": {
+                                    maxWidth: "160px"
+                                }
+                            }
+                        }
+                    }}
+                    label={localization.AggregateImportance.topKFeatures}
+                    min={minK}
+                    max={maxK}
+                    value={this.props.globalBarSettings.topK.toString()}
+                    onIncrement={this.setNumericValue.bind(this, 1, maxK, minK)}
+                    onDecrement={this.setNumericValue.bind(this, -1, maxK, minK)}
+                    onValidate={this.setNumericValue.bind(this, 0, maxK, minK)}
+                />
+                <Slider
+                    className={GlobalExplanationTab.classNames.startingK}
+                    ariaLabel={localization.AggregateImportance.topKFeatures}
+                    max={maxStartingK}
+                    min={0}
+                    step={1}
+                    value={this.props.globalBarSettings.startingK}
+                    onChange={this.setStartingK}
+                    showValue={true}
+                />
+            </div>
             <AccessibleChart
                 plotlyProps={plotlyProps}
                 sharedSelectionContext={undefined}
                 theme={this.props.theme}
+                relayoutArg={relayoutArg as any}
                 onSelection={undefined}
             />
             <DependencePlot 
@@ -57,11 +121,11 @@ export class GlobalExplanationTab extends React.PureComponent<IGlobalExplanation
                 metadata={this.props.metadata}
                 onChange={this.props.onDependenceChange}
             />
-        </>);
+        </div>);
     }
 
     private buildBarPlotlyProps(): IPlotlyProperty {
-        const sortedIndexVector = this.props.globalBarSettings.sortIndexVector.slice(-1 * this.props.globalBarSettings.topK).reverse();
+        const sortedIndexVector = this.props.globalBarSettings.sortIndexVector;
         const baseSeries = {
             config: { displaylogo: false, responsive: true, displayModeBar: false } as Plotly.Config,
             data: [],
@@ -114,12 +178,40 @@ export class GlobalExplanationTab extends React.PureComponent<IGlobalExplanation
         return baseSeries;
     }
 
+    private readonly setNumericValue = (delta: number, max: number, min: number, stringVal: string): string | void => {
+        const newProps = _.cloneDeep(this.props.globalBarSettings);
+        if (delta === 0) {
+            const number = +stringVal;
+            if (!Number.isInteger(number)
+                || number > max || number < min) {
+                return this.props.globalBarSettings.topK.toString();
+            }
+            newProps.topK = number;
+            this.props.onChange(newProps);
+        } else {
+            const prevVal = this.props.globalBarSettings.topK;
+            const newVal = prevVal + delta;
+            if (newVal > max || newVal < min) {
+                return prevVal.toString();
+            }
+            newProps.topK = newVal
+            this.props.onChange(newProps);
+        }
+    }
+
+    private setStartingK(newValue: number): void {
+        const newConfig = _.cloneDeep(this.props.globalBarSettings);
+        newConfig.startingK = newValue;
+        this.props.onChange(newConfig);
+    }
+
     private setDefaultSettings(props: IGlobalExplanationTabProps): void {
         const result: IGlobalBarSettings = {} as IGlobalBarSettings;
         result.topK = Math.min(this.props.jointDataset.localExplanationFeatureCount, 4);
+        result.startingK = 0;
         result.sortOption = "global";
         result.includeOverallGlobal = this.props.filterContext.filters.length > 0 || !this.props.isGlobalDerivedFromLocal;
-        result.sortIndexVector = [0,1,2,3,4];
+        result.sortIndexVector = ModelExplanationUtils.getSortIndices(this.props.globalImportance).reverse();
         this.props.onChange(result);
     }
 }
