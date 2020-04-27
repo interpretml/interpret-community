a705b89cf91cd8b13d885267342dbef07a5fedaf
Brandon Horn <rihorn@microsoft.com>
2020-04-27 16:18:12 -0400

    What if defaults to predicted prob

--- visualization/dashboard/src/MLIDashboard/Controls/WhatIfTab/WhatIfTab.tsx
+++ visualization/dashboard/src/MLIDashboard/Controls/WhatIfTab/WhatIfTab.tsx
@@ -976,7 +976,9 @@ export class WhatIfTab extends React.PureComponent<IWhatIfTabProps, IWhatIfTabSt
         const chartProps: IGenericChartProps = {
             chartType: ChartTypes.Scatter,
             xAxis: {
-                property: JointDataset.IndexLabel,
+                property: this.props.jointDataset.hasPredictedProbabilities ?
+                    JointDataset.ProbabilityYRoot + "0" :
+                    JointDataset.IndexLabel,
                 options: {}
             },
             yAxis: {
@@ -985,11 +987,6 @@ export class WhatIfTab extends React.PureComponent<IWhatIfTabProps, IWhatIfTabSt
                     dither: yIsDithered,
                     bin: false
                 }
-            },
-            colorAxis: {
-                property: this.props.jointDataset.hasPredictedY ?
-                    JointDataset.PredictedYLabel : JointDataset.IndexLabel,
-                options: {}
             }
         }
         this.props.onChange(chartProps);
