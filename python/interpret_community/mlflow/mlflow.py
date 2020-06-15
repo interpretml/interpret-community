5ff6adb6f4acb1215fa1bc4785f705d0743ef75d
Walter Martin <wamartin@microsoft.com>
2020-06-15 11:02:47 -0400

    add basic functions and test, not working

--- python/interpret_community/mlflow/mlflow.py
+++ python/interpret_community/mlflow/mlflow.py
@@ -1,8 +1,11 @@
 import mlflow
 from tempfile import TemporaryDirectory
 import os
+import yaml
 
-from ..explanation.explanation import save_explanation
+import interpret_community
+
+from ..explanation.explanation import save_explanation, load_explanation
 
 
 def _log_explanation(name, explanation):
@@ -10,3 +13,34 @@ def _log_explanation(name, explanation):
         path = os.path.join(tempdir, 'explanation')
         save_explanation(explanation, path)
         mlflow.log_artifacts(path)
+
+
+def _load_pyfunc(path):
+    load_explanation(path)
+
+
+def log_explanation(name, explanation):
+# def log_model(path, model):
+    try:
+        import mlflow.pyfunc
+    except ImportError as e:
+        raise Exception("Could not log_model to mlflow. Missing mlflow dependency, pip install mlflow to resolve the error: {}.".format(e))
+    import cloudpickle as pickle
+
+    with TemporaryDirectory() as tempdir:
+        path = os.path.join(tempdir, 'explanation')
+        save_explanation(explanation, path)
+
+        conda_env = {"name": "mlflow-env",
+                     "channels": ["defaults"],
+                     "dependencies": ["pip",
+                                      {"pip": [
+                                        "interpret-community=={}".format(interpret_community.VERSION),
+                                        "cloudpickle=={}".format(pickle.__version__)]
+                                      }
+                                     ]
+                    }
+        conda_path = os.path.join(tempdir, "conda.yaml")  # TODO Open issue and bug fix for dict support
+        with open(conda_path, "w") as stream:
+            yaml.dump(conda_env, stream)
+        mlflow.pyfunc.log_model(path, loader_module="interpret.glassbox.mlflow", data_path=tempdir, conda_env=conda_path)
--- test/mlflow/test_mlflow.py
+++ test/mlflow/test_mlflow.py
@@ -4,7 +4,7 @@ import mlflow
 
 from common_utils import create_sklearn_random_forest_classifier
 
-from interpret_community.mlflow.mlflow import _log_explanation
+from interpret_community.mlflow.mlflow import _log_explanation, log_explanation
 from interpret_community.explanation.explanation import load_explanation
 from constants import owner_email_tools_and_ux, DatasetConstants
 from test_serialize_explanation import _assert_explanation_equivalence
@@ -53,6 +53,25 @@ class TestMlflow(object):
         client = mlflow.tracking.MlflowClient()
         with mlflow.start_run() as run:
             _log_explanation(TEST_EXPLANATION, global_explanation)
+            os.mkdir(TEST_EXPLANATION)
+            download_path = client.download_artifacts(run.info.run_uuid, '', dst_path=TEST_EXPLANATION)
+        downloaded_explanation_mlflow = load_explanation(download_path)
+        _assert_explanation_equivalence(global_explanation, downloaded_explanation_mlflow)
+
+    def test_upload_as_model(self, iris, tabular_explainer, tracking_uri):
+        mlflow.set_tracking_uri(tracking_uri)
+        x_train = iris[DatasetConstants.X_TRAIN]
+        x_test = iris[DatasetConstants.X_TEST]
+        y_train = iris[DatasetConstants.Y_TRAIN]
+
+        model = create_sklearn_random_forest_classifier(x_train, y_train)
+
+        explainer = tabular_explainer(model, x_train)
+        global_explanation = explainer.explain_global(x_test)
+        mlflow.set_experiment(TEST_EXPERIMENT)
+        client = mlflow.tracking.MlflowClient()
+        with mlflow.start_run() as run:
+            log_explanation(TEST_EXPLANATION, global_explanation)
             os.mkdir(TEST_EXPLANATION)
             download_path = client.download_artifacts(run.info.run_uuid, '', dst_path=TEST_EXPLANATION)
         downloaded_explanation_mlflow = load_explanation(download_path)
