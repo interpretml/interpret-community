800caf1ec98885a8319044f4253369d28b75bcd0
Walter Martin <wamartin@microsoft.com>
2020-06-22 11:38:21 -0400

    haven't yet started on flavor-based approach

--- python/interpret_community/mlflow/mlflow.py
+++ python/interpret_community/mlflow/mlflow.py
@@ -3,6 +3,8 @@ from tempfile import TemporaryDirectory
 import os
 import yaml
 
+from mlflow.models import Model
+
 import interpret_community
 
 from ..explanation.explanation import save_explanation, load_explanation
@@ -26,6 +28,7 @@ def log_explanation(name, explanation):
         raise Exception("Could not log_model to mlflow. Missing mlflow dependency, pip install mlflow to resolve the error: {}.".format(e))
     import cloudpickle as pickle
 
+    model_explanation = Model()
     with TemporaryDirectory() as tempdir:
         path = os.path.join(tempdir, 'explanation')
         save_explanation(explanation, path, exist_ok=True)
@@ -42,5 +45,7 @@ def log_explanation(name, explanation):
         conda_path = os.path.join(tempdir, "conda.yaml")  # TODO Open issue and bug fix for dict support
         with open(conda_path, "w") as stream:
             yaml.dump(conda_env, stream)
-        mlflow.pyfunc.log_model(explanation.id, loader_module="interpret_community.mlflow", data_path=path, conda_env=conda_path)
-        # mlflow.pyfunc.add_to_model(explanation.id, loader_module="interpret_community.mlflow.mlflow")
+        mlflow.pyfunc.add_to_model(model_explanation, loader_module="interpret_community.mlflow", data_path=path, conda_env=conda_path, blah_blah={"foo": "bar"})
+        mlflow.pyfunc.save_model(os.path.join(tempdir, 'extra_temp'), loader_module="interpret_community.mlflow", data_path=path, conda_env=conda_path)
+        # mlflow.pyfunc.log_model(explanation.id, loader_module="interpret_community.mlflow", data_path=path, conda_env=conda_path)
+        Model.log(explanation.id, )
