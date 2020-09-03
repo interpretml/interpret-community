c6acf297d4f20c191c9ddd2e06682b68b17b51b0
Ilya Matiach <ilmat@microsoft.com>
2020-09-03 14:13:55 -0400

    remove visualization directory as it is being moved to a common responsible ai repository

--- visualization/dashboard/src/MLIDashboard/Controls/GlobalExplanationTab/IGlobalSeries.ts
+++
@@ -1,9 +0,0 @@
-export interface IGlobalSeries {
-    unsortedAggregateY: number[];
-    // feature x row, given how lookup is done
-    unsortedIndividualY?: number[][];
-    unsortedFeatureValues?: number[];
-    name: string;
-    colorIndex: number;
-    id?: number;
-}
