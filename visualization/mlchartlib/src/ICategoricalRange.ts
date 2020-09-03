7e29ca840d59fd495ac8d43e299dbe97755aaa85
Ilya Matiach <ilmat@microsoft.com>
2020-09-03 14:35:51 -0400

    remove visualization directory as it is being moved to a common responsible ai repository (#315)

--- visualization/mlchartlib/src/ICategoricalRange.ts
+++
@@ -1,6 +0,0 @@
-import { RangeTypes } from "./RangeTypes";
-
-export interface ICategoricalRange {
-    uniqueValues: string[];
-    rangeType: RangeTypes.categorical;
-}
--- visualization/mlchartlib/src/INumericRange.ts
+++
@@ -1,8 +0,0 @@
-import { RangeTypes } from "./RangeTypes";
-
-export interface INumericRange {
-    // if the feature is numeric
-    min: number;
-    max: number;
-    rangeType: RangeTypes.integer | RangeTypes.numeric;
-}
