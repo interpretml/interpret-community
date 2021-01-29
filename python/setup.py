2048414884170b1a11e82bd4f29d96901d507716
Ilya Matiach <ilmat@microsoft.com>
2021-01-28 21:38:43 -0500

    update to intepret-core 0.2.4 (#371)

--- python/setup.py
+++ python/setup.py
@@ -41,7 +41,7 @@ DEPENDENCIES = [
     'scipy',
     'scikit-learn',
     'packaging',
-    'interpret-core[required]>=0.1.20, <=0.2.1',
+    'interpret-core[required]>=0.1.20, <=0.2.4',
     'shap>=0.20.0, <=0.34.0'
 ]
 
