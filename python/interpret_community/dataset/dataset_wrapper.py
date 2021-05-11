57f32f93b4eba3f17aace9f807c1ffdc72410a06
Nanthini Balasubramanian <nathanb@nvidia.com>
2021-05-11 00:19:20 +0000

    Fix failing test
    Signed-off-by: Nanthini Balasubramanian <nathanb@nvidia.com>

--- python/interpret_community/dataset/dataset_wrapper.py
+++ python/interpret_community/dataset/dataset_wrapper.py
@@ -18,7 +18,10 @@ import warnings
 
 with warnings.catch_warnings():
     warnings.filterwarnings('ignore', 'Starting from version 2.2.1', UserWarning)
-    from shap.common import DenseData
+    try:
+        from shap.common import DenseData
+    except ImportError:
+        from shap.utils._legacy import DenseData
 
 SAMPLED_STRING_ROWS = 10
 
