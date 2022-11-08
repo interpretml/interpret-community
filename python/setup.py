add880f74f0025ad235e871c90a04800a9c39a88
Ilya Matiach <ilmat@microsoft.com>
2022-11-08 11:08:49 -0500

    update interpret-community to ml-wrappers 0.3.0

--- python/setup.py
+++ python/setup.py
@@ -39,7 +39,7 @@ DEPENDENCIES = [
     'numpy',
     'pandas',
     'scipy',
-    'ml-wrappers==0.2.2',
+    'ml-wrappers~=0.3.0',
     'scikit-learn',
     'packaging',
     'interpret-core[required]>=0.1.20, <=0.2.7',
