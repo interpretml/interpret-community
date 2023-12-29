c4f1897cd0d3d5bf30b573524a6efc1d6c4f0317
Ilya Matiach <ilmat@microsoft.com>
2023-12-29 12:03:10 -0500

    update interpret-community to shap 0.44.0

--- python/setup.py
+++ python/setup.py
@@ -43,7 +43,7 @@ DEPENDENCIES = [
     'scikit-learn',
     'packaging',
     'interpret-core[required]>=0.1.20, <=0.4.4',
-    'shap>=0.20.0, <=0.43.0',
+    'shap>=0.20.0, <=0.44.0',
     'raiutils~=0.4.0'
 ]
 
