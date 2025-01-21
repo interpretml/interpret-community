c73b0108ecd41005de913d7f2837da3e7226bde2
Ilya Matiach <ilmat@microsoft.com>
2025-01-21 11:38:42 -0500

    update interpret-community to python 3.9 by default

--- python/setup.py
+++ python/setup.py
@@ -26,8 +26,6 @@ CLASSIFIERS = [
     'Intended Audience :: Science/Research',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Topic :: Scientific/Engineering :: Artificial Intelligence',
     'Operating System :: Microsoft :: Windows',
